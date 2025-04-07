546e472d27327e95a7cbe32e739e66dc5905d0c0
Ximena mendoza <rotxipn@gmail.com>
2025-04-06 18:07:12 -0600

    v.0.1

---
+++ LunaShop/LunaShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LunaShop
+//
+//  Created by Ximena Rotceh Mendoza Gamino on 06/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LunaShop/LunaShop/LunaShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaShopApp.swift
+//  LunaShop
+//
+//  Created by Ximena Rotceh Mendoza Gamino on 06/04/25.
+//
+
+import SwiftUI
+
+@main
+struct LunaShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
